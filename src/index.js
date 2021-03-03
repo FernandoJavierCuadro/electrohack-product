a23df9c9e999f81d8323bae4402ff7873af3f91d
Fernando Javier Cuadro <68226901+FernandoJavierCuadro@users.noreply.github.com>
2021-03-03 16:43:02 -0300

    added conditional render, & buyFunction to onClick

--- src/index.js
+++ src/index.js
@@ -1,77 +1,89 @@
-import React, { Fragment } from "react";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import React, { Fragment } from 'react'
+import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-export const Product = ({ product, addToCart }) => {
-	const dispatch = useDispatch();
-	return (
-		<div className="col-md-6 col-lg-4 mb-3">
-			<div className="card my-2 shadow-about-us">
-				<Link
-					to={{
-						pathname:
-							"/products/" +
-							product.name.toLowerCase().trim().replace(/ /g, "-"),
-						state: { product },
-					}}
-				>
-					<div
-						className="img-card"
-						style={{
-							backgroundImage: `url(${product.image})`,
-						}}
-						alt="First slide"
-					></div>
-				</Link>
+export const Product = ({ product, buyFunction }) => {
+  const dispatch = useDispatch()
+  return (
+    <div className='col-md-6 col-lg-4 mb-3'>
+      <div className='card my-2 shadow-about-us'>
+        {product.name && (
+          <Link
+            to={{
+              pathname:
+                '/products/' +
+                product.name.toLowerCase().trim().replace(/ /g, '-'),
+              state: { product }
+            }}
+          >
+            <div
+              className='img-card'
+              style={{
+                backgroundImage: `url(${product.image})`
+              }}
+              alt='First slide'
+            ></div>
+          </Link>
+        )}
 
-				<div
-					className="card-body card-height second-color d-flex flex-column justify-content-between"
-					bg="snow"
-				>
-					<div>
-						<Link
-							to={{
-								pathname:
-									"/products/" +
-									product.name.toLowerCase().trim().replace(/ /g, "-"),
-								state: { product },
-							}}
-							className="text-dark"
-						>
-							<h5 className="card-title">{product.name}</h5>
-						</Link>
-					</div>
-					<div>
-						<img src={product.brand.logo} alt="" className="card-logo-size" />
-					</div>
+        <div
+          className='card-body card-height second-color d-flex flex-column justify-content-between'
+          bg='snow'
+        >
+          {product.name && (
+            <div>
+              <Link
+                to={{
+                  pathname:
+                    '/products/' +
+                    product.name.toLowerCase().trim().replace(/ /g, '-'),
+                  state: { product }
+                }}
+                className='text-dark'
+              >
+                <h5 className='card-title'>{product.name}</h5>
+              </Link>
+            </div>
+          )}
 
-					<div>
-						<p className="lead mb-0">$ {product.price}</p>
-						{product.stock > 0 ? (
-							<>
-								<p className="my-2">
-									<small>In stock</small>
-								</p>
-								<button
-									className="btn btn-custom btn-block"
-									onClick={() => dispatch(addToCart(product))}
-								>
-									<i className="fas fa-shopping-cart mr-2"></i> Add to Cart
-								</button>
-							</>
-						) : (
-							<>
-								<p className="my-2">
-									<small>Out of Stock</small>
-								</p>
-								<button disabled className="btn btn-custom btn-block">
-									<i className="fas fa-shopping-cart mr-2"></i> Add to Cart
-								</button>
-							</>
-						)}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-};
+          {product.brand.logo && (
+            <div>
+              <img src={product.brand.logo} alt='' className='card-logo-size' />
+            </div>
+          )}
+
+          <div>
+            {product.price && <p className='lead mb-0'>$ {product.price}</p>}
+
+            {product.stock && (
+              <>
+                {product.stock > 0 ? (
+                  <>
+                    <p className='my-2'>
+                      <small>In stock</small>
+                    </p>
+                    <button
+                      className='btn btn-custom btn-block'
+                      onClick={() => buyFunction(product)}
+                    >
+                      <i className='fas fa-shopping-cart mr-2'></i> Add to Cart
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <p className='my-2'>
+                      <small>Out of Stock</small>
+                    </p>
+                    <button disabled className='btn btn-custom btn-block'>
+                      <i className='fas fa-shopping-cart mr-2'></i> Add to Cart
+                    </button>
+                  </>
+                )}
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
