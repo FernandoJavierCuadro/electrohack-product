5beacec6c8634ca55538305506fed72eefcc8536
Fernando Javier Cuadro <68226901+FernandoJavierCuadro@users.noreply.github.com>
2021-03-03 16:53:49 -0300

    changed some dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React, { Fragment } from 'react'
-import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 export const Product = ({ product, buyFunction }) => {
-  const dispatch = useDispatch()
   return (
     <div className='col-md-6 col-lg-4 mb-3'>
       <div className='card my-2 shadow-about-us'>
