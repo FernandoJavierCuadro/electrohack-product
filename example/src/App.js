817be84bf65e30c7bce990dbf3834f7220671664
Fernando Javier Cuadro <68226901+FernandoJavierCuadro@users.noreply.github.com>
2021-03-04 09:59:12 -0300

    productCard export

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { Product } from 'electrohack-product'
+import { ProductCard } from 'electrohack-product'
 import 'electrohack-product/dist/index.css'
 
-const App = ({product, addToCart}) => {
-  return <Product product={product} addToCart={addToCart}/>
+const App = ({ product, addToCart }) => {
+  return <Product product={product} addToCart={addToCart} />
 }
 
 export default App
