ddb0b7a4f163ec21f12ad1a4ce67f43962276549
Fernando Javier Cuadro <68226901+FernandoJavierCuadro@users.noreply.github.com>
2021-03-04 10:48:59 -0300

    changed product to product card

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ProductCard } from 'electrohack-product'
 import 'electrohack-product/dist/index.css'
 
 const App = ({ product, addToCart }) => {
-  return <Product product={product} addToCart={addToCart} />
+  return <ProductCard product={product} addToCart={addToCart} />
 }
 
 export default App
